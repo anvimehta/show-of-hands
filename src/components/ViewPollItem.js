a89c40681475919b53f4e7968513bd777111e2f7
Jia Lin Cheoh <jcheoh@purdue.edu>
2019-04-08 23:42:02 -0400

    like backend

--- src/components/ViewPollItem.js
+++ src/components/ViewPollItem.js
@@ -5,35 +5,85 @@ import PollListItem from './PollListItem';
 import getPoll from '../selectors/get-poll';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import { startEditPoll } from '../actions/polls';
 
-export const ViewPollItem = ({ poll }) => {
-	if (!poll || !poll.id) { return null; }
-	const {
-		id, numberOfOptions, createdAt, description
-	} = poll;
+export class ViewPollItem extends React.Component {
+	constructor (props) {
+		super(props)
 
-	return (
-		<div>
-			<PollListItem data={poll} />;
-			{
-				poll && poll.editable ? <Link to={`/polls/${id}/edit`}>
-					<button>
-						Edit
+		this.toggleLikePoll = this.toggleLikePoll.bind(this)
+		this.likePoll = this.likePoll.bind(this)
+		this.unlikePoll = this.unlikePoll.bind(this)
+		this.likeCount = this.likeCount.bind(this)
+
+		this.state = {
+			poll_liked: this.isLikedAlready()
+		}
+	}
+	likeCount () {
+		return Object.keys(this.props.poll.likes || {}).length
+	}
+	isLikedAlready () {
+		return Object(this.props.poll.likes)[this.props.uid]
+	}
+	updateDb () {
+		startEditPoll(this.props.poll.id, {
+			likes: this.props.poll.likes 
+		})(function () {})
+	}
+	likePoll () {
+		this.props.poll.likes = Object(this.props.poll.likes)
+		this.props.poll.likes[this.props.uid] = Date.now()
+		this.updateDb();
+		this.setState({ poll_liked: true })
+	}
+	unlikePoll () {
+		delete this.props.poll.likes[this.props.uid]
+		this.updateDb();
+		this.setState({ poll_liked: false })
+	}
+	toggleLikePoll () {
+		if (this.isLikedAlready()) {
+			this.unlikePoll()
+		} else {
+			this.likePoll()
+		}
+	}
+	render () {
+		const { poll } = this.props;
+		if (!poll || !poll.id) { return null; }
+		const {
+			id, numberOfOptions, createdAt, description
+		} = poll;
+
+		return (
+			<div>
+				<PollListItem data={poll} />
+				{
+					poll && poll.editable ? <Link to={`/polls/${id}/edit`}>
+						<button>
+							Edit
+						</button>
+					</Link> : null
+				}
+				<Link to={`/polls/${id}/answer`}>
+					<button className="button">
+						Answer
 					</button>
-				</Link> : null
-			}
-			<Link to={`/polls/${id}/answer`}>
-				<button>
-					Answer
+				</Link>
+				{ " " }
+				<button className="button" onClick={this.toggleLikePoll}>
+					{ this.isLikedAlready() ? "Unlike" : "Like" }<br/>
+					<small>({this.likeCount()} likes)</small>
 				</button>
-			</Link>
-		</div>
-	);
-};
+			</div>
+		);
+	}
+}
 
 const mapStateToProps = (state) => {
 	const poll = getPoll(state.poll);
-	return { poll };
+	return { poll, uid: state.auth.uid };
 };
 
 export default connect(mapStateToProps)(ViewPollItem);
