dc27090ac33883555947fb3d6414662266bd2c83
Tanya Khandelwal <tkhandel@purdue.edu>
2019-04-18 21:02:24 -0400

    Filters

--- src/components/PollListFilters.js
+++ src/components/PollListFilters.js
@@ -72,7 +72,6 @@ export class PollListFilters extends React.Component {
         		/>
 				<br/><br/>
 				
-        		<br/><br/>
         		<select
         			onChange={this.onSortChange}
         		>
